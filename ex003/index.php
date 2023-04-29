af36368c4758a27d07e8122d76197f8a1af04900
Daniel Czastka <danielczastka.dax@gmail.com>
2023-04-29 11:32:06 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Premitivos</title>
+</head>
+<body>
+    <?php 
+    $num = 200;
+    // int
+
+    // 0x -> exadecimal / 0b -> binario / 0 -> octal
+    $exad = 0x1A;
+    // Exadecimal
+    
+    $str = "Daniel";
+    // String
+
+    $duble = 9.2;
+    $duble2 = 3e2;
+    // Dublle
+
+    $bul = true;
+    // Booleman
+
+
+
+
+
+
+?>
+</body>
+</html>
